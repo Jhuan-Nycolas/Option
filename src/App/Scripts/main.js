e2ad8b2354edcf16e7f664ee9a1bb4e6cdf06e28
Jhuan-Nycolas <jhuan2024@hotmail.com>
2024-11-19 22:14:46 -0300

    Mandando pro git

--- src/App/Scripts/main.js
+++ src/App/Scripts/main.js
@@ -3,14 +3,28 @@ const os = require("os")
 
 const explorer = document.querySelector(".explorer")
 let relativePath = "~"
-let item
 
-const clear() => {
+const clear = () => {
   explorer.innerHTML = ""
 }
 
-const add(name) => {
+const add = (name) => {
   const item = document.createElement("div")
   const img = document.createElement("img")
   const text = document.createElement("p")
+
+  let file = shell.cd(`${relativePath}/${name}`)
+  file = file.code
+
+  if (file == 0) {
+    alert("é uma pasta")
+  } else if (file != 0) {
+    alert("é um arquivo")
+  }
 }
+
+const dirs = shell.ls(relativePath)
+
+dirs.forEach(el => {
+  add(el)
+});
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "/App/index.html"));
-  mainWindow.setMenu(null);
+  //mainWindow.setMenu(null);
 };
 
 // This method will be called when Electron has finished
