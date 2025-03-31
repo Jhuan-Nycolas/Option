d747b86ff0a1aa8bdcef6faa4afa3d54f7f4ca0a
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-31 18:48:09 -0300

    Criando flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Option's Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.yarn
+          pkgs.nodejs
+        ];
+   
+        shellHook = "clear; ${pkgs.figlet}/bin/figlet Option Project DevShell Activated; exec fish";
+      };
+    };
+  };
+}
