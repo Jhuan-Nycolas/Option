3d4c0ef84a5986116858159773a175ce7acaa109
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-04-07 11:49:44 -0300

    Reiniciando em Java

--- Nix/Shell/flake.nix
+++ Nix/Shell/flake.nix
@@ -8,16 +8,14 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
 
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {inherit system;};
   in {
     devShells.${system} = {
       default = pkgs.mkShell {
         packages = [
-          pkgs.wails
-          pkgs.go
-          pkgs.nodejs
+          pkgs.jdk
         ];
-   
+
         shellHook = "clear; ${pkgs.figlet}/bin/figlet Option Project; exec fish";
       };
     };
